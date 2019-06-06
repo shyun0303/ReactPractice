1f7e801d447be467d56ed3003df0be63680e8676
sehyeon <shyun0303@gmail.com>
2019-06-06 23:01:42 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './Components/Customer';
+
+const customers = [{
+  'id':1,
+  "image":'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday': '940303',
+  'gender' : '남자',
+  'job' : '백수',
+},{
+  'id':2,
+  "image":'https://placeimg.com/64/64/2',
+  'name' : '바보',
+  'birthday': '930103',
+  'gender' : '여자',
+  'job' : '백수',
+},
+{
+  'id':3,
+  "image":'https://placeimg.com/64/64/3',
+  'name' : '멍청이',
+  'birthday': '980123',
+  'gender' : '남자',
+  'job' : '백수',
+},
+
+]
 
 class App extends Component{
   render(){
     return(
-      <div className = "gray-background">
-        <img src = {logo} lat = "logo"/>
-        
-      </div>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key = {c.id}     
+                id = {c.id}
+                image = {c.image}
+                name = {c.name}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />
+            );
+          })
+        }
+    </div>
     );
   }
 }
